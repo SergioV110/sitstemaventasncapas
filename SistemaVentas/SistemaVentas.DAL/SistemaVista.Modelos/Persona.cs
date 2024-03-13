98101037c577e69d4b926b643b3f4f7788e1a28e
Sergio <sergioleonelvelasquezortega8@gmail.com>
2024-03-12 21:34:40 -0400

    Proyecto base modificado

---
+++ SistemaVentas/SistemaVentas.DAL/SistemaVista.Modelos/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVista.Modelos
+{
+    public class Persona
+    {
+        public int IdPersona { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Telefono { get;set; }
+        public int Ci {  get; set; }
+        public string Correo { get; set; }
+        public string Estado { get; set; }
+
+    }
+}
